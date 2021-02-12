28740f02eab7b4f62d77d45670e2623fe1e5485e
unknown <samsonadgboyega@gmail.com>
2021-02-12 02:12:12 +0100

    added more components

--- src/components/generalComponents/Footer.js
+++ src/components/generalComponents/Footer.js
@@ -1,10 +1,59 @@
 import React, { Component } from 'react'
+import logo from '../../assets/images/medflit-logo.png';
+import fbIcon from '../../assets/images/fb.svg';
+import twitterIcon from '../../assets/images/twitter.svg';
+import igIcon from '../../assets/images/ig.svg';
+
+import { Container, Row, Col } from 'react-bootstrap'
 
 function Footer() {
     return (
-        <div>
-
-        </div>
+        <section className="footer">
+            <Container>
+                <Row>
+                    <Col md="5">
+                        <img src={ logo } width="120" />
+                        <Row className="mt-5">
+                            <img src={ fbIcon } width="" className="ml-2 social-icon" />
+                            <img src={ twitterIcon } width="" className="social-icon" />
+                            <img src={ igIcon } width="" className="social-icon" />
+                        </Row>
+                        <p className="copyright">© Medflit 2021. All rights reserved</p>
+                    </Col>
+                    <Col md="7">
+                        <Row>
+                            <Col md="4">
+                                <h5>Company</h5>
+                                <div className="mt-4">
+                                    <p>About</p>
+                                    <p>Testimonials</p>
+                                    <p>Find a doctor</p>
+                                    <p>FAQs</p>
+                                </div>                                    
+                            </Col>
+                            <Col md="4">
+                                <h5>Company</h5>
+                                <div className="mt-4">
+                                    <p>About</p>
+                                    <p>Testimonials</p>
+                                    <p>Find a doctor</p>
+                                    <p>FAQs</p>
+                                </div>                                    
+                            </Col>
+                            <Col md="4">
+                                <h5>Company</h5>
+                                <div className="mt-4">
+                                    <p>About</p>
+                                    <p>Testimonials</p>
+                                    <p>Find a doctor</p>
+                                    <p>FAQs</p>
+                                </div>                                    
+                            </Col>
+                        </Row>
+                    </Col>
+                </Row>
+            </Container>
+        </section>
     )
 }
 
--- src/components/homeComponents/WhyMedflit.js
+++ src/components/homeComponents/WhyMedflit.js
@@ -1,10 +1,81 @@
 import React, { Components } from 'react'
+import why from '../../assets/images/why1.png';
+
+import { Container, Row, Col } from 'react-bootstrap';
 
 function WhyMedflit() {
     return(
-        <div>
-
-        </div>
+        <section className="why-medflit">
+            <Container>
+                <Row className="mb-5">
+                    <Col>
+                        <h2>Why Medflit?</h2>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col md="3">
+                        <div className="medflit-item">
+                            <Row>
+                                <Col md="4">
+                                    <img src={ why } width="100" alt=""/>
+                                </Col>
+                            </Row>
+                            <Row>
+                                <Col>
+                                    <h5>Powerful</h5>
+                                    <p className="">Powerful technology that enables Video, Chat, and Audio Calls</p>
+                                </Col>
+                            </Row>
+                        </div>
+                    </Col>
+                    <Col md="3">
+                        <div className="medflit-item">
+                            <Row>
+                                <Col md="4">
+                                    <img src={ why } width="100" alt=""/>
+                                </Col>
+                            </Row>
+                            <Row>
+                                <Col>
+                                    <h5>Powerful</h5>
+                                    <p className="">Powerful technology that enables Video, Chat, and Audio Calls</p>
+                                </Col>
+                            </Row>
+                        </div>
+                    </Col>
+                    <Col md="3">
+                        <div className="medflit-item">
+                            <Row>
+                                <Col md="4">
+                                    <img src={ why } width="100" alt=""/>
+                                </Col>
+                            </Row>
+                            <Row>
+                                <Col>
+                                    <h5>Powerful</h5>
+                                    <p className="">Powerful technology that enables Video, Chat, and Audio Calls</p>
+                                </Col>
+                            </Row>
+                        </div>
+                    </Col>
+                    <Col md="3">
+                        <div className="medflit-item">
+                            <Row>
+                                <Col md="4">
+                                    <img src={ why } width="100" alt=""/>
+                                </Col>
+                            </Row>
+                            <Row>
+                                <Col>
+                                    <h5>Powerful</h5>
+                                    <p className="">Powerful technology that enables Video, Chat, and Audio Calls</p>
+                                </Col>
+                            </Row>
+                        </div>
+                    </Col>
+                </Row>
+            </Container>
+        </section>
     )
 }
 
